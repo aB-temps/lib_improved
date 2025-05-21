cce025b18cbf7c20f87b4d8e1c6c2481f3e28e34
aB-temps <abetemps@student.42lyon.fr>
2025-05-21 13:39:48 +0200

    ♻️  refactor(ft_bzero): optimized ft_bzero

--- sources/utils/ft_bzero.c
+++ sources/utils/ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: abetemps <abetemps@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 17:11:45 by abetemps          #+#    #+#             */
-/*   Updated: 2025/04/06 15:44:12 by abetemps         ###   ########.fr       */
+/*   Updated: 2025/05/21 13:38:55 by abetemps         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	size_t				count;
+	unsigned long long	*s_bis;
 
 	i = 0;
-	while (i < n)
+	count = 0;
+	s_bis = (unsigned long long *)s;
+	if (n >= sizeof(unsigned long long))
 	{
-		((unsigned char *)s)[i] = '\0';
-		i++;
+		while (count < n - sizeof(unsigned long long))
+		{
+			*(s_bis + i++) = 0;
+			count += sizeof(unsigned long long);
+		}
 	}
+	while (count < n)
+		*(((unsigned char *)s_bis) + count++) = 0;
 }
